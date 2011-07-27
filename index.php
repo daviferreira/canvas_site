ce3ecc28a3a411e25d8525893169cf640c6bac5e
daviferreira <contato@daviferreira.com>
2011-07-27 00:49:22 -0300

    Integrated with Wijmo

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
   <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
   <link rel="stylesheet" href="css/style.css?v=2">
+  <link href="http://cdn.wijmo.com/themes/rocket/jquery-wijmo.css" rel="stylesheet" type="text/css" />
+  <link href="http://cdn.wijmo.com/jquery.wijmo-open.1.1.6.css" rel="stylesheet" type="text/css" />
+  <link href="http://cdn.wijmo.com/jquery.wijmo-complete.1.1.6.css" rel="stylesheet" type="text/css" />
   <script src="js/libs/modernizr-1.7.min.js"></script>
 </head>
 
@@ -31,7 +34,7 @@
       
       <p id="intro"><img src="image.php?<?=date("U")?>" width="938" height="240" alt="Banksky!" /></p>
       
-      <p>Canvas is a PHP class to manipulate images, including <span>resize functions</span>, <span>filters</span>, <span>crop</span>, <span>conversion</span> and <span>method chaining</span>.</p>
+      <p>Canvas is a PHP class to manipulate images, including <strong>resize functions</strong>, <strong>filters</strong>, <strong>crop</strong>, <strong>conversion</strong> and <strong>method chaining</strong>.</p>
       
       <h3>How to use</h3>
       
@@ -60,7 +63,11 @@ $img->set_crop_coordinates(-400, -410)
     ->save("/tmp/new_image.png");
 </pre>
 
-      <p>For a full list of features, check the <a href="#">api documentation</a>.</p>
+      <p>For a full list of features, check the <button id="api">api documentation</button></p>
+      
+      <div id="api-dialog" style="display:none;">
+        <h2>API</h2>
+      </div>
 
       <h3>Requirements</h3>
       
@@ -74,12 +81,14 @@ $img->set_crop_coordinates(-400, -410)
       <h3>Examples</h3>
       
       <ul id="examples" class="clearfix">
-        <li><a href="">Simple resizes</a></li>
-        <li><a href="">Dynamic placeholder image</a></li>
-        <li class="end"><a href="">Truetype text</a></li>
-        <li><a href="">Dynamic thumbnails</a></li>
-        <li><a href="">Canvas and jCrop</a></li>
-        <li class="end"><a href="">Canvas and .htaccess</a></li>
+        <li><button href="">Simple resizes</button></li>
+        <li><button href="">Dynamic placeholder image</button></li>
+        <li class="end"><button href="">Truetype text</button></li>
+        <li><button href="">Dynamic thumbnails</button></li>
+        <li><button href="">Canvas and jCrop</button></li>
+        <li class="end"><button href="">Canvas and .htaccess</button></li>
+        <li><button href="">Canvas image editor</button></li>
+        <li><button href="">Dynamic image gallery</button></li>
       </ul>
       
       <h3>Troubleshooting</h3>
@@ -130,7 +139,16 @@ ini_set( 'display_startup_errors', true );
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
   <script>window.jQuery || document.write("<script src='js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script>
+  
+  <script src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.11/jquery-ui.min.js" type="text/javascript"></script>
+  <script src="http://cdn.wijmo.com/external/jquery.bgiframe-2.1.3-pre.js" type="text/javascript"></script>
+  <script src="http://cdn.wijmo.com/external/jquery.glob.min.js" type="text/javascript"></script>
+  <script src="http://cdn.wijmo.com/external/jquery.mousewheel.min.js" type="text/javascript"></script>
+  <script src="http://cdn.wijmo.com/external/raphael-min.js" type="text/javascript"></script>
+  <script src="http://cdn.wijmo.com/jquery.wijmo-open.1.1.6.min.js" type="text/javascript"></script>
+  
   <script src="js/libs/jquery.snippet.min.js"></script>
+
   <script>
   $('pre').each(function(){
     $(this).snippet("php",{
--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 /* Author: 
-
+  Davi Ferreira <contato@daviferreira.com>
 */
+
+$(function(){  
+  $('#api-dialog').wijdialog({
+    autoOpen: false,
+    width:960,
+    height:500
+  });
+
+  $('#api').button().click(function(e){
+    $('#api-dialog').wijdialog('open')
+    e.preventDefault();
+  });
+  
+  $('#examples button').button();
+});
 
 
 
