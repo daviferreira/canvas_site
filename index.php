a96c518592304543fb56e21f7a6415fdf7c080da
daviferreira <contato@daviferreira.com>
2011-08-01 23:40:02 -0300

    Analytics

--- index.php
+++ index.php
@@ -165,10 +165,8 @@ ini_set( 'display_startup_errors', true );
     <script>DD_belatedPNG.fix("img, .png_bg");</script>
   <![endif]-->
 
-
-
   <script>
-    var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID 
+    var _gaq=[["_setAccount","UA-24891590-1"],["_trackPageview"]];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
     g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
     s.parentNode.insertBefore(g,s)}(document,"script"));
